46d2319256d6fd28ac33e71fb67cf20fcc08e7cf
Jonathan <jonathan.leeercier@gmail.com>
2024-03-28 12:45:14 +0100

    <feat>(Style navbar) : AJout de style pour la navbar

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import '../../assets/styles/navbar.css';
+import LogoAsterix from '../../assets/images/logo_asterix.png';
 // Définition du composant sous forme de fonction
 function MonComposant() {
     return (
-      <nav>
+        
+      <nav className='nav-container'>
         <ul>
             <li>
-                <img src="" alt="" />
+                <img src={LogoAsterix} alt="" className='logo-asterix' />
                 <a href="/">Accueil</a>
                 <a href="/">Attractions</a>
                 <a href="/">Aide</a>
                 <a href="/">Contact</a>
-                
             </li>
         </ul>
       </nav>
