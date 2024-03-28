dab1b92bbd6410bab6daa886b8617728f5e911dc
Jonathan <jonathan.leeercier@gmail.com>
2024-03-28 12:12:12 +0100

    <feat>(Création Composant) : Création du Composant de la navbar sous React

---
+++ client/src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// Définition du composant sous forme de fonction
+function MonComposant() {
+    return (
+      <div>
+        <h1>Mon Composant</h1>
+        <p>Ceci est un exemple de composant React.</p>
+      </div>
+    );
+  }
+  
+  // Export du composant pour qu'il soit utilisable ailleurs
+  export default MonComposant;
