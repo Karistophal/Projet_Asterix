09dac951ce4024f23a75248a725a74d7f7ae7075
Charles Delfau <charlesdelfau@gmail.com>
2024-04-04 11:01:20 +0200

    <feat>structureApp : modification html css

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
+import Home from './pages/home/Home.js';
+import Navbar from './components/navbar.jsx';
 
 
 function App() {
   return (
       <Router>
-        <Navbar />
-        <div>
+        <Navbar></Navbar>
+        <div className='app'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/list" element={""} />
