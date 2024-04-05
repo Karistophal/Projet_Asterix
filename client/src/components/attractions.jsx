5a8c7f10e46e7d3777f89612926130f75058c21b
Jonathan <jonathan.leeercier@gmail.com>
2024-04-05 14:16:47 +0200

    <feat>(création composant) : Création du composant des attractions

---
+++ client/src/components/attractions.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/attractions.css';
+
+function attractions(){
+    return (
+        <div className="attractions_container">
+        </div>
+      );
+}
+export default attractions;
