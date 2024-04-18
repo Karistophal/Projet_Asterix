4ed2de198ba5687adbd82d38268192912ebc2fb0
Lemercier Jonathan <jonathan.leercier@gmail.com>
2024-04-18 20:44:17 +0200

    <feat> (ajout tri) : ajout de conditions pour le tri

--- client/src/components/filtre.jsx
+++ client/src/components/filtre.jsx
@@ -9,7 +9,8 @@ import '../assets/styles/filter.css';
 function Filtre({data, setFilterdata}) {
     // Initialisation de l'état sliderValue à 0 en utilisant le hook useState
     const [sliderValue, setSliderValue] = useState(0);
-
+    const [isAscending, setIsAscending] = useState(true);
+    const [isAlphabetical, setIsAlphabetical] = useState(true);
     // Définition d'une fonction pour gérer les changements de l'input de recherche
     const handleChange = (e) => {
         // Récupération de la valeur de l'input de recherche
@@ -27,10 +28,13 @@ function Filtre({data, setFilterdata}) {
     // Définition d'une fonction pour trier les données par nom
     const handleSort = () => {
         // Tri des données
-        const sortedData = [...data].sort((a, b) => a.nom.localeCompare(b.nom));
-
+        const sortedData = [...data].sort((a, b) => isAlphabetical ? a.nom.localeCompare(b.nom) : b.nom.localeCompare(a.nom));
+    
         // Mise à jour des données filtrées avec les données triées
         setFilterdata(sortedData);
+    
+        // Inversion de l'ordre de tri pour le prochain clic
+        setIsAlphabetical(!isAlphabetical);
     }
 
     // Définition d'une fonction pour gérer les changements du slider
@@ -49,12 +53,23 @@ function Filtre({data, setFilterdata}) {
         // Mise à jour des données filtrées
         setFilterdata(result);
     }
+    const handleSortBySize = () => {
+        // Tri des données par taille
+        const sortedData = [...data].sort((a, b) => isAscending ? a.tailleMini - b.tailleMini : b.tailleMini - a.tailleMini);
+    
+        // Mise à jour des données filtrées avec les données triées
+        setFilterdata(sortedData);
+    
+        // Inversion de l'ordre de tri pour le prochain clic
+        setIsAscending(!isAscending);
+    }
 
     // Rendu du composant
     return (
         <div className='filter-container'> 
             <input  type="text" name='name' onChange={handleChange}  className="form-control" placeholder='Search...' />
             <button onClick={handleSort}>Trier par nom</button>
+            <button onClick={handleSortBySize}>Trier par taille</button>
             Taille Minimum : <input type="range" min="0" max="200" value={sliderValue} onChange={handleSliderChange} />
             <output>{sliderValue}</output>
         </div>
