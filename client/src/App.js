a6d498697d752bb9cafdbe42687df9de678829f6
Jonathan <jonathan.leercier@gmail.com>
2024-04-18 11:21:31 +0200

    <dev> (ajout route) : direction routeur

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home.js';
+import Home from './pages/home.jsx';
 import Navbar from './components/navbar.jsx';
 import Attractions from './pages/attractions.jsx';
 
@@ -10,6 +10,7 @@ function App() {
         <Navbar></Navbar>
         <div className='app'>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/attractions" element={<Attractions />} />
         </Routes>
       </div>
